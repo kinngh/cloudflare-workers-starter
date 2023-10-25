687f8561da249398820193d3295922eddc9c9db7
kinngh <kinngh@me.com>
2023-10-25 17:24:01 +0530

    🧹 Cleanup

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Learning Cloudflare Workers
+# Cloudflare Workers Starter
 
-Learning Cloudflare workers with `Wrangler` and `itty-router` to create a pure serverless stack for creating Shopify applications.
+Cloudflare Workers starter kit that comes with `wrangler` and `itty-router`, and a bunch of examples on how to get started with basic routing.
 
-- Using Prisma with Cloudflare worker: https://www.prisma.io/docs/guides/deployment/deployment-guides/deploying-to-cloudflare-workers
+## Docs
+
 - Itty-router: https://github.com/kwhitley/itty-router
 - Itty-router-extras: https://github.com/kwhitley/itty-router-extras
 - Cloudflare Worker documentation: https://developers.cloudflare.com/workers/
--- index.js
+++ index.js
@@ -4,36 +4,31 @@ import { json } from "itty-router-extras";
 
 const router = Router();
 
+//Simple route that returns a text
 router.get("/", (request) => {
   console.log(request);
   return new Response("This is a working Cloudflare worker");
 });
 
+//Simple route that returns a JSON
 router.get("/json-return", (request) => {
   console.log(request);
   return json({ message: "This is a JSON response" });
 });
 
-function functionRoute(request) {
-  console.log(typeof request);
-  return new Response("This is coming from a function");
+//Middleware + Routing
+
+function thisIsMiddleware() {
+  console.log("--> running middleware");
+  //   return new Response("404, not found!", { status: 404 });
 }
-
-function noParamFunction() {
-  return new Response("This is coming from no param function");
-}
-
-router.get("/fn", (request) => {
-  return functionRoute(request);
-});
-
-router.get("/noparam", noParamFunction);
 
 router.get("/go", thisIsMiddleware, (request) => {
   console.log(request);
   return new Response("This is my second route");
 });
 
+//Route + Function + No params
 router.get("/new-route", newRouteFunction); //(request) is automatically passed to the function, just need to include it in params
 
 function newRouteFunction(request) {
@@ -41,16 +36,12 @@ function newRouteFunction(request) {
   return new Response("Yeet");
 }
 
-function thisIsMiddleware() {
-  console.log("--> running middleware");
-  //   return new Response("404, not found!", { status: 404 });
-}
-
-// HTTP 307 status code is for temporary redirect.
-// DO NOT USE 301 BECAUSE IT'S FOR PERMANENT REDIRECT
-router.get("/rere", (request) => {
-  const base = "https://harshdeephura.com";
-  const statusCode = 307; //307 = temporary redirect
+//307 Temporary redirect
+router.get("/redirect-route", (request) => {
+  const base = "https://x.com/kinngh";
+  // HTTP 307 status code is for temporary redirect.
+  // DO NOT USE 301 BECAUSE IT'S FOR PERMANENT REDIRECT
+  const statusCode = 307;
 
   return Response.redirect(base, statusCode);
 });
